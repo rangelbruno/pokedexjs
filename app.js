204a05469d688e559e1eb084a1a8410707d59694
rangelbruno <brunojp.rangel@gmail.com>
2020-03-17 20:59:56 -0300

    Last version

--- app.js
+++ app.js
@@ -1,33 +1,28 @@
-const fetchPokemon = () => {
-    const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
 
-    const pokemonPromisses = []
+const generatePokemonPromisses = () => Array(150).fill().map((_, index) =>
+    fetch(getPokemonUrl(index + 1)).then(response => response.json()))
+    
+const generateHTML = pokemons => pokemons.reduce((accumulator, { name, id, types }) => {
+    const elementTypes = types.map(typeInfo => typeInfo.type.name)
 
-    for (let i = 1; i <= 150; i++) {
-       pokemonPromisses.push(fetch(getPokemonUrl(i)).then(response => response.json())) 
-    }
-
-    Promise.all(pokemonPromisses)
-        .then(pokemons => {
-            //console.log(pokemons)
-
-            const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
-                const types = pokemon.types.map(typeInfo => typeInfo.type.name)
-
-                accumulator += `
-                    <li class="card ${types[0]}">
-                    <img class="card-image" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png">
-                        <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
-                        <p class="card-subtitle">${types.join(' | ')}</p>    
-                    </li>
-                `
-                return accumulator
-            }, '')
-
-            const ul = document.querySelector('[data-js="pokedex"]')
-
-            ul.innerHTML = lisPokemons
-        })
+    accumulator += `
+        <li class="card ${elementTypes[0]}">
+            <img class="card-image" alt="${name}" src="https://pokeres.bastionbot.org/images/pokemon/${id}.png">
+            <h2 class="card-title">${id}. ${name}</h2>
+            <p class="card-subtitle">${elementTypes.join(' | ')}</p>    
+        </li>
+    `
+    return accumulator
+}, '')
+    
+const insertPokemonsIntoPage = pokemons => {
+    const ul = document.querySelector('[data-js="pokedex"]')
+    ul.innerHTML = pokemons
 }
 
-fetchPokemon()
+const pokemonPromisses = generatePokemonPromisses()
+
+Promise.all(pokemonPromisses)
+    .then(generateHTML)
+    .then(insertPokemonsIntoPage)
